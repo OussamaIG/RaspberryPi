8e79e4776c2182e6ef2474c853ed6090f1c0cce8
OussamaIG <oussig7@yahoo.com>
2023-05-29 01:53:05 +0300

    fffff

--- audio.py
+++ audio.py
@@ -1,41 +1,36 @@
 import sounddevice as sd
+import soundfile as sf
 import speech_recognition as sr
-import numpy as np
-import io
 
 def record_audio(duration):
-    print("Recording started. Speak into the microphone...")
-    audio = sd.rec(int(duration * 44100), samplerate=44100, channels=1)
-    sd.wait()  # Wait until recording is complete
-    print("Done")
-    return audio
+    # Set the sample rate and channels for recording
+    sample_rate = 44100
+    channels = 1
 
+    # Record audio for the specified duration
+    recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels)
+    sd.wait()
 
-def convert_audio_to_text(audio):
+    return recording, sample_rate
+
+def convert_audio_to_text(audio_file):
     r = sr.Recognizer()
-    audio_data = audio.flatten()
     text = ""
 
-    audio_file = io.BytesIO()
-    np.save(audio_file, audio_data)
-    audio_file.seek(0)
-
     with sr.AudioFile(audio_file) as audio_src:
-        audio_data = audio_file.read()
-        audio_src.write_frames(audio_data)
-
-        try:
-            audio = r.record(audio_src)
-            text = r.recognize_google(audio)
-        except sr.UnknownValueError:
-            print("Speech recognition could not understand audio")
-        except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service: {e}")
+        audio = r.record(audio_src)
+        text = r.recognize_google(audio)
 
     return text
 
+# Record audio for 5 seconds
+duration = 5
+audio, sample_rate = record_audio(duration)
 
-record_duration = 5  # Specify the duration of the recording in seconds
-audio = record_audio(record_duration)
-text = convert_audio_to_text(audio)
-print("Transcription:", text)
+# Save the recorded audio as a WAV file
+file_path = "recorded_audio.wav"
+sf.write(file_path, audio, sample_rate)
+
+# Convert the audio file to text
+text = convert_audio_to_text(file_path)
+print(text)
