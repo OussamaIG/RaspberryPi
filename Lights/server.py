d5f5f932f220202e17ec3332f94149d4181000b3
OussamaIG <oussig7@yahoo.com>
2023-05-30 21:23:29 +0300

    sss

--- Lights/server.py
+++ Lights/server.py
@@ -58,12 +58,12 @@ def screendisplay(msg, msg2):
     font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
 
 
-    text_x = 10
-    text_y = 10
+    text_x = 30
+    text_y = 30
 
     t_start = time.time()
 
-    img = img.rotate(90).resize((WIDTH, HEIGHT))
+    img = img.rotate(180).resize((WIDTH, HEIGHT))
     draw.text((text_x, text_y), MESSAGE, font=font, fill=(255, 255, 255))
     disp.display(img)
     
